6857b4a51dbd965a0f521306400279b3b9706a90
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-02-04 05:23:27 -0800

    Add files via upload
    2nd js assgiment

---
+++ app.js
@@ -0,0 +1,46 @@
+// chapter # 2 Assgiment 2
+
+// question 1
+// var username =("what is your name")
+// alert(username)
+
+// question 2
+
+// var myName =("My name is Gohar Abbas");
+// alert(myName);
+
+// question 3
+
+// var message =("Hello World");
+// alert(message);
+
+// question 4
+
+// var studentData =("Gohar Abbas");
+// alert(studentData);
+// alert("I have done intermediate");
+// alert("I from pakistan");
+
+// question 5
+
+// var one =("PIZZA \n PIZZ \n PIZ \n PI \n p");
+// alert(one);
+
+// question 6
+
+// var email =("goharabbasrizvi98@gmail.com");
+// alert("Its My Email  " +email);
+
+// question 7
+
+// var book =("A smarter way to learn JavaScript");
+// alert("i am typing to learn from the book " +book);
+
+// question 8
+
+// var a =("Yah! can write HTML content through JavaScript");
+// document.write(a);
+
+// question 9
+var box =("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+alert(box);
